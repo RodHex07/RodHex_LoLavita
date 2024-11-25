87d29fb89ddf70098279b579caa91a59f3a9e272
RodHex <ozgncefe@gmail.com>
2024-11-25 19:57:10 +0300

    Update Main.js

--- Script/Main.js
+++ Script/Main.js
@@ -39,5 +39,5 @@ document.addEventListener("DOMContentLoaded", () => {
         setTimeout(() => {
             card.classList.remove('fade-out');
         }, 8000); // 4000ms = 4 saniye
-    }, 11000); // 10000ms = 10 saniye
+    }, 13000); // 10000ms = 10 saniye
 });
